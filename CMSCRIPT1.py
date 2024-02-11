07dcc88b743432bea09645ac68bc98e3badd309a
cmedina13 <145918480+cmedina13@users.noreply.github.com>
2024-02-11 15:31:54 -0400

    Create CMSCRIPT1.py

---
+++ CMSCRIPT1.py
@@ -0,0 +1,7 @@
+def multiply_two_numbers(a, b):
+    return a * b
+num1 = int(input("Primer Numero a Multiplicar: "))
+num2 = int(input("Segundo Numero a Multiplicar: "))
+result = multiply_two_numbers(num1, num2)
+print(f"Oh!, {num1} por {num2} es {result}")
+salir = int(input("Good!... Ahora CM dice: Presione cualquier tecla para salir"))
